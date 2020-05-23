f0d7c8c6a1fa446f61392694ea23c7887b643e8a
Source Solutions <zx@itch.io>
2020-05-23 20:21:13 +0100

    updated text for next release

--- basic/modules/13_data.asm
+++ basic/modules/13_data.asm
@@ -27,7 +27,7 @@ copyright:
 	defb "SE BASIC IV 4.2 Cordelia", ctrl_enter;
 	defb "Copyright (C)2017 Source Solutions, Inc.", ctrl_enter;
 	defb ctrl_enter;
-;	defb "Release 200523", ctrl_enter;
+;	defb "Release 200612", ctrl_enter; Zhora
  	defb TIMESTR, ctrl_enter;
 	defb ctrl_enter, 0;
 
