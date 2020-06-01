94aac27c4a106ace205b3c44660de1151befd0a2
Source Solutions <zx@itch.io>
2020-06-01 16:14:58 +0100

    Updated Chloe VM for Windows

--- ChloeVM.app/ChloeVM.cmd
+++ ChloeVM.app/ChloeVM.cmd
@@ -1,2 +1,2 @@
 cd Contents\Resources
-start /min "Console" "%CD%\chloe.exe" --noconfigfile --enable-mmc --enable-divmmc --mmc-file empty.img --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
+start /min "Console" "%CD%\chloevm.exe" --noconfigfile --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
