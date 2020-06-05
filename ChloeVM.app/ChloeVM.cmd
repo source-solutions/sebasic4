a22b1bcd29521226eeab2a449b2b0176d1bad330
Source Solutions <zx@itch.io>
2020-06-05 14:13:06 +0100

    Added NMI on F1 to VM

--- ChloeVM.app/ChloeVM.cmd
+++ ChloeVM.app/ChloeVM.cmd
@@ -1,2 +1,2 @@
 cd Contents\Resources
-start /min "Console" "%CD%\chloevm.exe" --noconfigfile --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
+start /min "Console" "%CD%\chloevm.exe" --noconfigfile --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
