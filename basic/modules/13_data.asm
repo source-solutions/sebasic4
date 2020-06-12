9bab35e68c5bed77b4b7d36469998198364b0338
Andrew Owen <aowen@mi9retail.com>
2020-06-12 13:04:27 +0100

    Removed Zeus datestamp

--- basic/modules/13_data.asm
+++ basic/modules/13_data.asm
@@ -28,8 +28,7 @@ copyright:
 	defb "Copyright (C)2020 Source Solutions, Inc.", ctrl_enter;
 	defb ctrl_enter;
 ;	defb "Release 200715", ctrl_enter;	// Morton
-; 	defb "YY-MM-DD HH:MM", ctrl_enter;
- 	defb "10:56 pm Sun 7/Jun/2020", ctrl_enter;
+ 	defb "YY-MM-DD HH:MM", ctrl_enter;
 	defb ctrl_enter, 0;
 
 bytes_free:
