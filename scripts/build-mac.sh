d55adcf137f82c37aa21b6b2cccbd5ccea79d77a
Source Solutions <aowen10@mail.bbk.ac.uk>
2020-06-12 15:28:59 +0100

    Added Debian-32 target

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -1,10 +1,10 @@
 cd basic
-../rasm/rasm -pasmo basic.asm -ob ../bin/23.bin
+../rasm/rasm-mac -pasmo basic.asm -ob ../bin/23.bin
 cd ../bin
 head -c 16384 23.bin > basic.rom
 tail -c 7168 23.bin > ../boot/basic.bin
 cd ../boot
-../rasm/rasm -pasmo boot.asm -ob ../bin/boot.rom
+../rasm/rasm-mac -pasmo boot.asm -ob ../bin/boot.rom
 rm basic.bin
 cp ../bin/boot.rom ../ChloeVM.app/Contents/Resources/se.rom
 cat ../bin/basic.rom >> ../ChloeVM.app/Contents/Resources/se.rom
---
+++ scripts/run-deb.sh
@@ -0,0 +1,2 @@
+cd ChloeVM.app/Contents/Resources/
+./chloevm-deb --noconfigfile --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
