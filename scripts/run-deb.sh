f1a334bcf0c9d691712dee36ad0cbe50f9cb18d8
Source Solutions <zx@itch.io>
2020-06-23 23:12:35 +0100

    Fixed emulator in emulator

--- scripts/run-deb.sh
+++ scripts/run-deb.sh
@@ -1,2 +1,2 @@
 cd ChloeVM.app/Contents/Resources/
-./chloevm-deb --noconfigfile --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
+./chloevm-deb --noconfigfile --enable-mmc --enable-divmmc --mmc-file empty.img --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
