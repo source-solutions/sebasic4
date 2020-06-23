a5b8ca020c0aca321664486d5d8762c5181abd90
Source Solutions <zx@itch.io>
2020-06-23 15:19:56 +0100

    NTSC mode / LASTV8 DEMO

--- basic/modules/13_data.asm
+++ basic/modules/13_data.asm
@@ -29,7 +29,7 @@ copyright:
 	defb ctrl_cr;
 ;	defb "Release 200715", ctrl_cr;	// Morton
 ;	defb "YY-MM-DD HH:MM", ctrl_cr;
-	defb "20-06-23 00:20", ctrl_cr;
+	defb "20-06-23 15:00", ctrl_cr;
 	defb ctrl_cr, 0;
 
 bytes_free:
