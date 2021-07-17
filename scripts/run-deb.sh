befc93708fb94aed9dcd931816343f5edd64ab73
Andrew Owen <aowen10@mail.bbk.ac.uk>
2021-07-17 18:00:43 +0000

    Added Linux-arm64 to build scripts

--- scripts/run-deb.sh
+++ scripts/run-deb.sh
@@ -1,2 +1,2 @@
 cd ChloeVM.app/Contents/Resources/
-./chloevm-deb --noconfigfile --enable-mmc --enable-divmmc --mmc-file empty.img --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
+./chloevm-deb-arm64 --noconfigfile --enable-mmc --enable-divmmc --mmc-file empty.img --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
