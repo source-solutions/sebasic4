1ae4e6ed23fb4eb7bf06b7a32d02eaf3f7fa5e80
Daniel A. Nagy <nagy.da@gmail.com>
2022-01-15 20:38:54 +0100

    Changing to i386 rasm and fuse for emulation

--- scripts/run-deb.sh
+++ scripts/run-deb.sh
@@ -1,2 +1,4 @@
-cd ChloeVM.app/Contents/Resources/
-./chloevm-deb-arm64 --noconfigfile --enable-mmc --enable-divmmc --mmc-file empty.img --def-f-function f1 NMI --def-f-function f4 Reset --enablekempstonmouse --enable-esxdos-handler --esxdos-root-dir chloehd --machine Chloe280 --disablefooter --nowelcomemessage --quickexit --nosplash --gui-style QL --menucharwidth 6 --zoomx 4 --zoomy 3
+#! /bin/sh
+
+fuse -mse --rom-spec-se-0 bin/boot.rom --rom-spec-se-1 bin/basic.rom
+
