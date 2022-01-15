47a17cf01b6babd6f3b9b9334d64828a87ee409c
Andrew Owen <aowen10@mail.bbk.ac.uk>
2022-01-15 19:10:18 +0000

    set no filesystem switches

--- basic/basic.asm
+++ basic/basic.asm
@@ -19,8 +19,8 @@
 
 	include "basic.inc";				// label definitions and X80 instruction set
 
-;	slam equ 1;							// uncomment to build SLAM+128/divMMC version
-;   no_fs equ 1;						// uncomment to build without filesystem support to debug the interpreter with fuse-emulator
+	slam equ 1;							// uncomment to build SLAM+128/divMMC version
+	no_fs equ 1;						// uncomment to build without filesystem support to debug the interpreter with fuse-emulator
 
 ;	// export program in separate ROM and RAM segments
 
