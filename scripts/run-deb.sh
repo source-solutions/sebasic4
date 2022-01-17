40f9912701113f8406085ccdc35464d0a070fbf6
Andrew Owen <aowen10@mail.bbk.ac.uk>
2022-01-17 23:15:46 +0000

    basck to i386 for dn

--- scripts/run-deb.sh
+++ scripts/run-deb.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 
-fuse -mse --rom-spec-se-0 bin/boot.rom --rom-spec-se-1 bin/basic.rom
-
+fuse -mse --rom-spec-se-0 bin/boot.rom --rom-spec-se-1 bin/basic.rom --snapshot fuse/se4.szx --divmmc-file fuse/se4.hdf
