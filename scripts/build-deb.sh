308a6b27aff1b96e3d34eafd4ca8b7d255f7ec5a
Andrew Owen <aowen10@mail.bbk.ac.uk>
2022-01-18 12:06:15 +0000

    updated linux build script for multi-arch

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,11 +1,28 @@
+arch=$(arch)
+
 cd basic
-../rasm/rasm-deb-i386 -pasmo basic.asm -ob ../bin/23.bin -sz -os ../bin/symbols.txt
+
+if [[ "$arch" == "aarch64" ]];
+then ../rasm/rasm-deb-arm64 -pasmo basic.asm -ob ../bin/23.bin -sz -os ../bin/symbols.txt
+else ../rasm/rasm-deb-i386 -pasmo basic.asm -ob ../bin/23.bin -sz -os ../bin/symbols.txt
+fi
+
 cd ../boot
-../rasm/rasm-deb-i386 -pasmo boot.asm -ob ../bin/boot.rom
+
+if [[ "$arch" == "aarch64" ]];
+then ../rasm/rasm-deb-arm64 -pasmo boot.asm -ob ../bin/boot.rom
+else ../rasm/rasm-deb-i386 -pasmo boot.asm -ob ../bin/boot.rom
+fi
+
 rm basic.bin
 cp ../bin/boot.rom ../ChloeVM.app/Contents/Resources/se.rom
 cat ../bin/basic.rom >> ../ChloeVM.app/Contents/Resources/se.rom
 cd ../rasm
-./rasm-deb-i386 firmware.asm -ob ../bin/FIRMWA~1.BIN
+
+if [[ "$arch" == "aarch64" ]];
+then ../rasm-deb-arm64 firmware.asm -ob ../bin/FIRMWA~1.BIN
+else ../rasm-deb-i386 firmware.asm -ob ../bin/FIRMWA~1.BIN
+fi
+
 cd ..
 ./scripts/run-deb.sh
