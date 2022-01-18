ee89ab6ec4e4814c1c3230854b30e226d613ee98
Andrew Owen <aowen10@mail.bbk.ac.uk>
2022-01-18 00:06:37 +0000

    minimal unodos3 szx file

--- scripts/run-deb.sh
+++ scripts/run-deb.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-fuse -mse --rom-spec-se-0 bin/boot.rom --rom-spec-se-1 bin/basic.rom --snapshot fuse/se4.szx --divmmc-file fuse/se4.hdf
+fuse -mse --rom-spec-se-0 bin/boot.rom --rom-spec-se-1 bin/basic.rom --snapshot fuse/unodos3.szx --divmmc-file fuse/fat-16.hdf
