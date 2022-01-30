20f361d4a6dd951aa291da6a0d08ec263668a1be
Andrew Owen <aowen10@mail.bbk.ac.uk>
2022-01-30 13:10:17 +0000

    WHILE/WEND error messages

--- basic/modules/13_data.asm
+++ basic/modules/13_data.asm
@@ -63,12 +63,13 @@ ifdef slam
 endif
 
 	defb ctrl_cr;
-	defb "SE BASIC IV 4.2 Cordelia", ctrl_cr;
+	defb "SE BASIC IV 4.2-b9 Cordelia", ctrl_cr;
 	defb "Copyright (C)2022 Source Solutions, Inc.", ctrl_cr;
 	defb ctrl_cr;
 	timestamp 'YY-MM-DD h:m';			// RASM directive
-;	defb "Release YYMMDD";				// 
-	defb ctrl_cr, ctrl_cr, 0;
+	defb ctrl_cr;
+;	defb "GPL-3.0 License", ctrl_cr;
+	defb ctrl_cr, 0;
 
 bytes_free:
 	defb " BASIC bytes free", ctrl_cr;
