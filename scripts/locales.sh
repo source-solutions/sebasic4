4bbfa20d0ec1c17e844cb64f18db19e1c8088a3e
Cheveron <aowen10@mail.bbk.ac.uk>
2022-02-11 00:51:44 +0000

    Fixed script for later versions of iconv

--- scripts/locales.sh
+++ scripts/locales.sh
@@ -36,6 +36,7 @@ for f in *.json; do
     input=$(jq -r .INPUT $jname)
     path=$(jq -r .PATH $jname)
     echo $scroll"\0"$error"\0"$ready"\0"$ok"\0"$break"\0"$for"\0"$synatx"\0"$gosub"\0"$data"\0"$call"\0"$overflow"\0"$memory"\0"$line"\0"$subscript"\0"$variable"\0"$address"\0"$statement"\0"$type"\0"$screen"\0"$device"\0"$stream"\0"$channel"\0"$function"\0"$buffer"\0"$next"\0"$wend"\0"$while"\0"$file"\0"$input"\0"$path"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0" > TEMP.LN
+    perl -pi -e 's/\\0/\0/g' $fname
     iconv -f UTF8 -t $iconv TEMP.LN > $fname
     head -c 608 $fname > TEMP.LN
     mv TEMP.LN $fname
